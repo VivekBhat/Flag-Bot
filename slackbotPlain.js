39919321d797de4a8c310cf9f8142455fcecaaaf
kebrey <kebrey@ncsu.edu>
2016-10-18 00:37:36 -0400

    work on slackbot

--- slackbotPlain.js
+++ slackbotPlain.js
@@ -1,52 +1,56 @@
 var SlackBot = require('slackbots');
+var notificationChannels = ["C2QHSD89J"];
+var sendAsUser = false;
 
 // create a bot 
 var bot = new SlackBot({
     // Add a bot https://my.slack.com/services/new/bot and put the token  
-    token: token, 
-    name: 'weatherbot'
+    token: "", 
+    name: 'FlagLag Bot'
 });
  
-bot.on('start', function() {
-    // more information about additional params https://api.slack.com/methods/chat.postMessage 
-    var params = {
-        icon_emoji: ':cat:'
-    };
-    
-    // define channel, where bot exist. You can adjust it there https://my.slack.com/services  
-    // define existing username instead of 'user_name' 
-    bot.postMessageToUser('katiebrey', 'meow!', params); 
-    bot.getChannels().then(function(channels)
-    {
-        console.log(JSON.stringify(channels, null, 3))
-    });
+//TODO: change image
+bot.on('start', function() {    
+    notify(getCommands());
 });
 
 bot.on('message', function(data) {
     // all ingoing events https://api.slack.com/rtm 
-    if( data.type == 'message' && getUser(data.user).name != bot.name )
+    // Check that it is a message type, not a bot, and the user is not the bot
+    if( data.type == 'message' && data.user && getUser(data.user).name != bot.name )
     {
-		var message = data.text;
+        if(getChannel(data.channel) && !data.text.includes("<@" + bot.self.id + ">")) {
+            return;
+        }
+
+        var command = data.text.toLowerCase().trim();
 		
-		// List flags
-        if( message.contains('list flags') )
-        {
-			//TODO
-			reply(data, "Don't tell me what to do!");
-        } 
-		
-		//Create flag
-		else if( message.contains('create flag') ) {
-			//TODO
-			reply(data, "Don't tell me what to do!");
-		} 
-		
-		else {
-			reply(data, "Are you talking to me? Please type 'help' to " +
-			"see the list of commands I will respond to.");
-		}
+        switch(command) {
+            case 'list flags':
+                reply(data, "Don't tell me what to do!");
+                break;
+            case 'create flag':
+                reply(data, "Don't tell me what to do!");
+                break;
+            case 'delete flag':
+                reply(data, "Don't tell me what to do!");
+                break;
+            default :
+                reply(data,getCommands());
+        }
     }
 });
+
+//Posts message to notificationChannels array defined at top
+function notify(msg) {
+    for(var i = 0; i < notificationChannels.length; i++) {
+        var channel = getChannel(notificationChannels[i]);
+        if( channel ) 
+        {
+            bot.postMessageToChannel(channel.name, msg, {as_user: sendAsUser});    
+        }
+    }
+}
 
 function reply(data, msg)
 {
@@ -54,12 +58,12 @@ function reply(data, msg)
     if( channel )
     {
         //console.log( "replying in channel ")
-        bot.postMessageToChannel(channel.name, msg, {as_user: true});    
+        bot.postMessageToChannel(channel.name, msg, {as_user: sendAsUser});    
     }
     else
     {
         var user = getUser(data.user)
-        bot.postMessageToUser(user.name, msg, {as_user: true} );
+        bot.postMessageToUser(user.name, msg, {as_user: sendAsUser} );
     }
 }
 
@@ -77,4 +81,17 @@ function getUser(userId)
     {
         return item.id === userId;
     })[0];
+}
+
+//Usage:
+    // more information about additional params https://api.slack.com/methods/chat.postMessage 
+    // var params = {
+    //     icon_emoji: ':cat:'
+    // };
+    // bot.getChannels().then(function(channels)
+    // {
+    //     console.log(JSON.stringify(channels, null, 3))
+    // });
+function getCommands() {
+    return "Here are your options.\n\nThat's right, none. Sorry.";
 }
